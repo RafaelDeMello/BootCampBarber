4950a963bff1f4ae06e39be25628e3f1fd4d0f46
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-08-13 02:30:58 -0300

    Commit2
    Criação de Servidor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Person } = require("./person");
 
 // require("./modules/path");
 // require("./modules/fs");
+// require("./modules/http");
 
-require("./modules/fs");
-
+require("./modules/express");
 const person = new Person("Felipe");
