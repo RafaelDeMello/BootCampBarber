8bc0f105434d548c529ece3db25617d115f30426
Rafael de Mello <rafaeldemello2023@gmail.com>
2025-08-13 19:14:39 -0300

    Commit Final BackeEnd Test

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const connectToDatabase = require("./src/database/connect");
 
 dotenv.config();
 
+require("./modules/express");
+
 connectToDatabase();
+
+
 
 // require("./modules/path");
 // require("./modules/fs");
 // require("./modules/http");
-// require("./modules/express");
 // const person = new Person("Felipe");
--- src/database/connect.js
+++ src/database/connect.js
@@ -1,19 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSOWORD}@cursonodejsdicasparadev.vqacf2i.mongodb.net/?retryWrites=true&w=majority&appName=CursoNodeJsDicasparaDevs`,
-    (error) => {
-      if (error) {
-        return console.log(
-          "Ocorreu um erro ao se conectar ao banco de dados:",
-          error
-        );
-      }
-
-      return console.log("Conexão ao banco de dados realizada com sucesso!");
-    }
-  );
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsdicasparadev.vqacf2i.mongodb.net/?retryWrites=true&w=majority&appName=CursoNodeJsDicasparaDevs`
+    );
+    console.log("Conexão ao banco de dados realizada com sucesso!");
+  } catch (error) {
+    console.log("Ocorreu um erro ao se conectar ao banco de dados:", error);
+  }
 };
 
 module.exports = connectToDatabase;
